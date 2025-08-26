d4147b38d308936ec9f610b8c19a160a75ee5954
SolutionsCProjetos <SolutionsCProjetos@hotmail.com>
2025-08-26 18:42:53 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://63.142.255.87:3008/:path*', // backend HTTP
+      },
+    ];
+  },
 };
 
 export default nextConfig;
